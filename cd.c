1e2588a0b1d52067177ee830c7df08e6a94717b2
icmp <icmp@icmps.org>
2013-07-05 00:09:33 -0400

    added a bunch of stuff like getopt() and a main() i/o loop to process and parse commands from stdin or a file also moved the old test commands into test.txt for -f test.txt testing
    just need to figure out the clr()/ncurses stuff and start fork/execl'ing other commands...then we can do pause()
    too

--- cd.c
+++ cd.c
@@ -2,8 +2,11 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdlib.h>
 
+FILE* input;
 char* current_dir;
+char* current_cmd;
 
 // http://pubs.opengroup.org/onlinepubs/9699919799/functions/chdir.html#tag_16_57_06_01
 int cd(const char* path) {
@@ -48,8 +51,8 @@ int ls_path(const char* path) {
     return ret;
 }
 
-int ls() {
-    return ls_path(current_dir);
+int ls(char *path) {
+    return ls_path(path);
 }
 
 int pwd() {
@@ -77,6 +80,12 @@ int pause() {
     return 0;
 }
 
+void quit(int code) {
+	// XXX kill children
+
+	exit(code);
+}
+
 char *make_prompt(const char *format) {
     char prompt[1024] = "";
     char temp[1024] = "";
@@ -99,32 +108,78 @@ char *make_prompt(const char *format) {
     return prompt;
 }
 
-int main() {
+// copied a strncmp() implementation but return a pointer to the remainder of string after matching the beginning to cmd
+char *test_cmd(const char *buf, const char *cmd) {
+	int c1, c2;
+
+	if (buf == NULL || cmd == NULL) return NULL;
+
+	do {
+		c1 = *buf++;
+		c2 = *cmd++;
+	} while ( (c1 == c2) && c1 != 0 && c2 != 0);
+
+	if (c2 == 0 && c1 != 0) return buf;
+
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
     const char format[1024] = "gcli\\%%uid/%%pwd> ";
-    char *ptr, *prompt;
-    char temp[1024] = "", buf[1024] = "";
-    extern FILE *stdin;
+    char buf[1024] = "";
+    char *ptr;
 
+    // XXX track open children for pause() etc
+    pid_t children[1024];
     current_dir = getcwd(NULL, 1024);
+    input = stdin; // Until getopt tells us differently
 
-    prompt = make_prompt(format);
-
+    int flags, opt;
+    while ((opt = getopt(argc, argv, "vf:")) != -1) {
+	switch (opt) {
+	case 'v':
+		printf("ghettocli: 0.1\n");
+		exit(0);
+		break;
+	case 'f':
+		input = fopen(optarg, "r+");
+		if (input == NULL) err("fopen");
+		break;
+	default:
+		fprintf(stderr, "Usage: %s [-v] [-f FILENAME]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+    }
+    
     printf("hai\n");
 
-    pwd();
-    ls();
-    cd("/Users/");
-    ls();
-    cd("/wioejfowiejf"); 
-    ls_path("/wefwefw");
-    cd("test");
-    pwd();
+    do {
+	buf[0] = 0; // clear previous line
+	if (input == stdin) printf("%s", make_prompt(format));
+	fgets(buf, 1024, input);
 
-    prompt = make_prompt(format);
-    printf("%s", prompt);
-    fgets(buf, 1024, stdin);
+	if (buf == NULL || strlen(buf) <= 0) break;
 
-    echo(buf);
+	buf[strlen(buf)-1] = 0; // kill trailing \n
+
+	// XXX hmm should we implement a case-insensitive strcmp?
+	if (strcmp(buf, "quit") == 0) quit(0);
+
+	// pwd, cd, ls, dir
+	current_cmd = buf;
+
+	if (strcmp(buf, "pwd") == 0) pwd();
+	else if (strcmp(buf, "ls") == 0) ls(current_dir);
+	else if (strcmp(buf, "dir") == 0) ls(current_dir);
+	else if ((ptr=test_cmd(buf, "cd")) != NULL) cd(ptr);
+	else if ((ptr=test_cmd(buf, "ls")) != NULL) ls(ptr);
+	else if ((ptr=test_cmd(buf, "dir")) != NULL) ls(ptr);
+	else echo(buf);
+
+    }
+    while (1);
+
+    fclose(input);
 
     return 0;
 
